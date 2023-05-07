fb3be90a1a4088fbadfee6f23ac38e31c9abfb4e
Rohit kumar <91061651+RohitAmdahl@users.noreply.github.com>
2023-05-08 00:51:56 +0200

    fetched cards

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import Title from "./components/ui/AppTitle";
 import PhotoCards from "./components/ui/PhotoCards";
 import search from "./images/search.svg";
 
-const ApiUrl = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo&pretty=true`;
+const ApiUrl = `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=yellow+flowers&image_type=photo&pretty=true`;
 
 function App() {
+  // eslint-disable-next-line no-unused-vars
   const [images, setImages] = useState([]);
-  const [searchTerm, setSearchTerm] = useState("");
+  // const [searchTerm, setSearchTerm] = useState("");
 
-  const ImagesGallery = async (tags) => {
-    const response = await fetch(`${ApiUrl}&s=${tags}`);
-    const data = await response.json();
-    console.log(data);
-    setImages(data.hits);
-  };
   useEffect(() => {
-    ImagesGallery("birds");
+    const ImagesGallery = async (tags) => {
+      const response = await fetch(`${ApiUrl}&s=${tags}`);
+      const data = await response.json();
+      const picture = data.hits;
+      console.log(picture);
+      setImages(picture);
+    };
+    ImagesGallery();
   }, []);
+
   return (
     <>
       <Title />
@@ -27,17 +30,20 @@ function App() {
           className="border-2 focus:border-sky-500 w-1/2 py-2 mr-2 px-2 shadow-lg rounded-full"
           type="text"
           placeholder="Search photos"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
         />
         <img
           className="w-10 h-10 drop-shadow-2xl cursor-pointer px-1 bg-green-300 rounded-full shadow-lg hover:bg-green-500"
           src={search}
           alt="search icon"
-          onClick={() => ImagesGallery(searchTerm)}
         />
       </div>
-      <PhotoCards />
+      <div className="container mx-auto flex justify-items-center">
+        <div className="grid grid-cols-1 gap-3 md:grid-cols-2 lg:grid-cols-3 mx-auto max-w-5xl ">
+          {images.map((images) => (
+            <PhotoCards key={images.id} image={images} />
+          ))}
+        </div>
+      </div>
     </>
   );
 }
--- src/components/ui/PhotoCards.jsx
+++ src/components/ui/PhotoCards.jsx
@@ -1,43 +1,48 @@
-const PhotoCards = () => {
+import React from "react";
+
+const PhotoCards = ({ image }) => {
+  const tags = image.tags.split(",");
   return (
     <div className="shadow-lg max-w-sm">
       <img
         className="w-full py-3 px-3 lg:max-w-sm"
-        src="https://source.unsplash.com/random"
+        src={image.webformatURL}
         alt="random"
       />
       <div className="px-3 py-3">
         <p className="text-green-800 font-bold text-2xl capitalize mb-2">
-          photo by rohit
+          photo by : {image.user}
         </p>
-        <p className="text-grey-800 lg:max-w-sm">
+        {/* <p className="text-grey-800 lg:max-w-sm">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum optio
           nulla distinctio deleniti maxime inventore, vero doloribus beatae
           nesciunt, asperiores harum similique odit ea unde, autem culpa alias
           provident voluptates.
-        </p>
+        </p> */}
         <ul className="">
           <li>
-            <strong>View: </strong>4900
+            <strong>View: </strong>
+            {image.views}
           </li>
           <li>
-            <strong>Download: </strong>4800
+            <strong>Download: </strong>
+            {image.downloads}
           </li>
           <li>
-            <strong>Likes: </strong>600
+            <strong>Likes: </strong>
+            {image.likes}
           </li>
         </ul>
       </div>
-      <div className="py-3 px-3">
-        <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 font-bold text-gray-700 mr-2">
-          photos
-        </span>
-        <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 font-bold text-gray-700 mr-2">
-          photos
-        </span>
-        <span className=" inline-block bg-gray-200 rounded-full px-3 py-1 font-bold text-gray-700 mr-2">
-          photos
-        </span>
+      <div className="py-3 px-3 grid grid-cols-2 gap-2">
+        {tags.map((tags) => (
+          <span
+            key={image.id}
+            className=" inline-block bg-gray-200 rounded-2xl px-3 m-1 py-2 font-bold text-gray-700 mr-2"
+          >
+            #{tags}
+          </span>
+        ))}
       </div>
     </div>
   );
